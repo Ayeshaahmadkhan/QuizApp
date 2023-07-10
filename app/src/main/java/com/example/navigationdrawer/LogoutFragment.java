7c69db48332a612a849a66877d96b57367ec76cf
Ayesha <ayeshaahmadkhan8827@gmail.com>
2023-07-10 18:01:07 +0500

    Initial main activity commit

---
+++ app/src/main/java/com/example/navigationdrawer/LogoutFragment.java
@@ -0,0 +1,43 @@
+package com.example.navigationdrawer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class LogoutFragment extends Fragment {
+
+    public LogoutFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_logout, container, false);
+
+        Button logoutButton = view.findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Exit the app
+                getActivity().finish();
+            }
+        });
+
+        return view;
+    }
+}
